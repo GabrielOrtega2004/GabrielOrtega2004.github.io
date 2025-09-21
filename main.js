351d73a40c093496deac4dfc7095a8dd52732057
GabrielOrtega2004 <159990642+GabrielOrtega2004@users.noreply.github.com>
2025-09-21 15:59:32 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow, shell } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('index.html');
+
+  // 👉 Esto permite abrir enlaces externos en el navegador por defecto
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(createWindow);
